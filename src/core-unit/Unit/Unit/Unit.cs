92b8adcdf2c589b86e498225087c87315e21b7b8
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-02 14:31:23 +0300

    Invert inherited interfaces declaration order (for performance reasons)

--- src/core-unit/Unit/Unit/Unit.cs
+++ src/core-unit/Unit/Unit/Unit.cs
@@ -1,5 +1,8 @@
 ﻿namespace System;
 
-public readonly partial struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
+public readonly partial struct Unit :
+    IComparable,
+    IComparable<Unit>,
+    IEquatable<Unit>
 {
 }
