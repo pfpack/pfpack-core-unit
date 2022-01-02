11be48a6a8708a65d0c0ce1cc3898546fcedae00
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-01-02 22:57:53 +0300

    Revert "Invert inherited interfaces declaration order (for performance reasons)"
    This reverts commit 92b8adcdf2c589b86e498225087c87315e21b7b8.

--- src/core-unit/Unit/Unit/Unit.cs
+++ src/core-unit/Unit/Unit/Unit.cs
@@ -1,8 +1,5 @@
 ﻿namespace System;
 
-public readonly partial struct Unit :
-    IComparable,
-    IComparable<Unit>,
-    IEquatable<Unit>
+public readonly partial struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
 {
 }
