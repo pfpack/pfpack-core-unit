fa74b8e76d52fdc8bae0af7de1172f87425a68d9
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-05-23 23:21:03 +0400

    Add UnitJsonConverter

--- src/core-unit/Unit/Unit/Unit.cs
+++ src/core-unit/Unit/Unit/Unit.cs
@@ -1,5 +1,8 @@
-﻿namespace System;
+﻿using System.Text.Json.Serialization;
 
+namespace System;
+
+[JsonConverter(typeof(UnitJsonConverter))]
 public readonly partial struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
 {
 }
---
+++ src/core-unit/Unit/UnitJsonConverter/UnitJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace System;
+
+internal sealed class UnitJsonConverter : JsonConverter<Unit>
+{
+    public override Unit Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        =>
+        default;
+
+    public override void Write(Utf8JsonWriter writer, Unit value, JsonSerializerOptions options)
+    {
+        Debug.Assert(writer is not null);
+
+        writer.WriteStartObject();
+        writer.WriteEndObject();
+    }
+}
