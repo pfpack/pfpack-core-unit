767f4279e2f2d9cda979bdad0fc7a61be1873326
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-08 23:12:48 +0400

    Arrange code

--- src/core-unit/Unit/Unit/Unit.Parse.cs
+++ src/core-unit/Unit/Unit/Unit.Parse.cs
@@ -6,25 +6,25 @@ partial struct Unit
 		=>
 		default;
 
+	static Unit ISpanParsable<Unit>.Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
+		=>
+		default;
+
+	static Unit IUtf8SpanParsable<Unit>.Parse(ReadOnlySpan<byte> utf8Text, IFormatProvider? provider)
+		=>
+		default;
+
 	static bool IParsable<Unit>.TryParse(string? s, IFormatProvider? provider, out Unit result)
 	{
 		result = default;
 		return true;
 	}
 
-	static Unit ISpanParsable<Unit>.Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
-		=>
-		default;
-
 	static bool ISpanParsable<Unit>.TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, out Unit result)
 	{
 		result = default;
 		return true;
 	}
-
-	static Unit IUtf8SpanParsable<Unit>.Parse(ReadOnlySpan<byte> utf8Text, IFormatProvider? provider)
-		=>
-		default;
 
 	static bool IUtf8SpanParsable<Unit>.TryParse(ReadOnlySpan<byte> utf8Text, IFormatProvider? provider, out Unit result)
 	{
