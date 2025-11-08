3030e00db8cae4f96beee0f13ea64c6d198e12ea
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-09 01:47:40 +0400

    Minor refactor tests

--- src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.Common.cs
+++ src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.Common.cs
@@ -1,21 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace PrimeFuncPack.Core.Tests;
 
 partial class UnitFormatTests
 {
 	public static TheoryData<string?> ParseCases
-#pragma warning disable IDE0028 // Simplify collection initialization
-#pragma warning disable IDE0305 // Simplify collection initialization
 		=>
-		new(EnumerateParseCases().ToArray());
+		new([.. EnumerateParseCases()]);
 
 	public static TheoryData<string> ParseCasesWithoutNull
 		=>
-		new(EnumerateParseCasesWithoutNull().ToArray());
-#pragma warning restore IDE0305 // Simplify collection initialization
-#pragma warning restore IDE0028 // Simplify collection initialization
+		new([.. EnumerateParseCasesWithoutNull()]);
 
 	private static IEnumerable<string?> EnumerateParseCases()
 		=>
