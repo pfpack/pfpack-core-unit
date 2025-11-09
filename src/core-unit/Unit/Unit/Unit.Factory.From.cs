abe83fd995838e1069a41faca65e0d9e5c7180be
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-10 02:25:32 +0400

    Fix formatting (restore spaces instead of tabs)

--- src/core-unit/Unit/Unit/Unit.Factory.From.cs
+++ src/core-unit/Unit/Unit/Unit.Factory.From.cs
@@ -6,8 +6,8 @@ namespace System;
 
 partial struct Unit
 {
-	// We maintain that Unit can be derived from any input value.
+    // We maintain that Unit can be derived from any input value.
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static Unit From<TResult>(TResult result) => default;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Unit From<TResult>(TResult result) => default;
 }
