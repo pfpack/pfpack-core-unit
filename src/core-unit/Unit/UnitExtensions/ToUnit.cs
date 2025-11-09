e6178c161b6c9f0caed11501f3e20afaddf390ff
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-10 02:10:25 +0400

    Clean a redundant comment

--- src/core-unit/Unit/UnitExtensions/ToUnit.cs
+++ src/core-unit/Unit/UnitExtensions/ToUnit.cs
@@ -2,8 +2,6 @@
 
 partial class UnitExtensions
 {
-    // We maintain that Unit can be derived from any input value.
-
     public static Unit ToUnit<TResult>(this TResult result)
         =>
         Unit.From(result);
