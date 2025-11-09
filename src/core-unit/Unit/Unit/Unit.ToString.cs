0927723011f6cdd009d244e97bcc31fde0d2db59
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-09 19:17:29 +0400

    Add redirect to the Format method

--- src/core-unit/Unit/Unit/Unit.ToString.cs
+++ src/core-unit/Unit/Unit/Unit.ToString.cs
@@ -1,8 +1,22 @@
-﻿namespace System;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System;
 
 partial struct Unit
 {
     public override string ToString()
         =>
         UnitForms.General;
+
+    // Redirect to the Format method.
+
+    [Obsolete(NotIntendedMessage_ToStringFormattable, error: true)]
+    [DoesNotReturn]
+    public string ToString(string? format)
+        =>
+        throw new NotImplementedException(NotIntendedMessage_ToStringFormattable);
+
+    private const string NotIntendedMessage_ToStringFormattable
+        =
+        $"This method is not intended for use. Call {nameof(Format)} instead.";
 }
