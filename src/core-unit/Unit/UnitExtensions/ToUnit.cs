0d6bb66659af2222012bd5541a49f511aca577c2
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-10 02:08:13 +0400

    Remove a redundant AggressiveInlining

--- src/core-unit/Unit/UnitExtensions/ToUnit.cs
+++ src/core-unit/Unit/UnitExtensions/ToUnit.cs
@@ -1,12 +1,9 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace System;
+﻿namespace System;
 
 partial class UnitExtensions
 {
-	// We maintain that Unit can be derived from any input value.
+    // We maintain that Unit can be derived from any input value.
 
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Unit ToUnit<TResult>(this TResult result)
         =>
         Unit.From(result);
