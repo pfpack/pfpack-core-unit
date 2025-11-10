255f2a28bc3ab713ab7536f6abc5dd3f19f2cb58
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-11 00:19:47 +0400

    Restore ToUnit extension diff

--- src/core-unit/Unit/UnitExtensions/ToUnit.cs
+++ src/core-unit/Unit/UnitExtensions/ToUnit.cs
@@ -1,7 +1,10 @@
-﻿namespace System;
+﻿using System.Runtime.CompilerServices;
+
+namespace System;
 
 partial class UnitExtensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Unit ToUnit<TResult>(this TResult result)
         =>
         Unit.From(result);
