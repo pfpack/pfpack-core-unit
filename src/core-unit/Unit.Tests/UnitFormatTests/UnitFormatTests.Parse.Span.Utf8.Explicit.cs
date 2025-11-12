227a50e1b666a7049e13de2c5222dc31e89c6b90
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-12 22:16:20 +0400

    Fix formatting (restore spaces instead of tabs) - test

--- src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.Span.Utf8.Explicit.cs
+++ src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.Span.Utf8.Explicit.cs
@@ -6,38 +6,38 @@ namespace PrimeFuncPack.Core.Tests;
 
 partial class UnitFormatTests
 {
-	[Theory]
-	[MemberData(nameof(ParseCases))]
-	public static void ParseSpanUtf8Explicit_Succeeds(string? s)
-		=>
-		Inner_ParseSpanUtf8Explicit_Succeeds<Unit>(s);
+    [Theory]
+    [MemberData(nameof(ParseCases))]
+    public static void ParseSpanUtf8Explicit_Succeeds(string? s)
+        =>
+        Inner_ParseSpanUtf8Explicit_Succeeds<Unit>(s);
 
-	[Theory]
-	[MemberData(nameof(ParseCases))]
-	public static void TryParseSpanUtf8Explicit_Succeeds(string? s)
-		=>
-		Inner_TryParseSpanUtf8Explicit_Succeeds<Unit>(s);
+    [Theory]
+    [MemberData(nameof(ParseCases))]
+    public static void TryParseSpanUtf8Explicit_Succeeds(string? s)
+        =>
+        Inner_TryParseSpanUtf8Explicit_Succeeds<Unit>(s);
 
-	private static void Inner_ParseSpanUtf8Explicit_Succeeds<TUnit>(string? s)
-		where TUnit : struct, IUtf8SpanParsable<TUnit>
-	{
-		ReadOnlySpan<byte> span = s is null ? null : Encoding.UTF8.GetBytes(s);
-		var actual = TUnit.Parse(span, null);
+    private static void Inner_ParseSpanUtf8Explicit_Succeeds<TUnit>(string? s)
+        where TUnit : struct, IUtf8SpanParsable<TUnit>
+    {
+        ReadOnlySpan<byte> span = s is null ? null : Encoding.UTF8.GetBytes(s);
+        var actual = TUnit.Parse(span, null);
 
-		var expected = default(TUnit);
+        var expected = default(TUnit);
 
-		Assert.StrictEqual(expected, actual);
-	}
+        Assert.StrictEqual(expected, actual);
+    }
 
-	private static void Inner_TryParseSpanUtf8Explicit_Succeeds<TUnit>(string? s)
-		where TUnit : struct, IUtf8SpanParsable<TUnit>
-	{
-		ReadOnlySpan<byte> span = s is null ? null : Encoding.UTF8.GetBytes(s);
-		var actual = TUnit.TryParse(span, null, out var result);
+    private static void Inner_TryParseSpanUtf8Explicit_Succeeds<TUnit>(string? s)
+        where TUnit : struct, IUtf8SpanParsable<TUnit>
+    {
+        ReadOnlySpan<byte> span = s is null ? null : Encoding.UTF8.GetBytes(s);
+        var actual = TUnit.TryParse(span, null, out var result);
 
-		var expected = default(TUnit);
+        var expected = default(TUnit);
 
-		Assert.True(actual);
-		Assert.StrictEqual(expected, result);
-	}
+        Assert.True(actual);
+        Assert.StrictEqual(expected, result);
+    }
 }
--- src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.String.Explicit.cs
+++ src/core-unit/Unit.Tests/UnitFormatTests/UnitFormatTests.Parse.String.Explicit.cs
@@ -5,63 +5,63 @@ namespace PrimeFuncPack.Core.Tests;
 
 partial class UnitFormatTests
 {
-	[Theory]
-	[MemberData(nameof(NotNullParseCases))]
-	public static void ParseStringExplicit_Succeeds(string s)
-		=>
-		Inner_ParseStringExplicit_Succeeds<Unit>(s);
+    [Theory]
+    [MemberData(nameof(NotNullParseCases))]
+    public static void ParseStringExplicit_Succeeds(string s)
+        =>
+        Inner_ParseStringExplicit_Succeeds<Unit>(s);
 
-	[Theory]
-	[MemberData(nameof(NotNullParseCases))]
-	public static void TryParseStringExplicit_Succeeds(string s)
-		=>
-		Inner_TryParseStringExplicit_Succeeds<Unit>(s);
+    [Theory]
+    [MemberData(nameof(NotNullParseCases))]
+    public static void TryParseStringExplicit_Succeeds(string s)
+        =>
+        Inner_TryParseStringExplicit_Succeeds<Unit>(s);
 
-	[Fact]
-	public static void ParseStringExplicit_NullInput_Fails()
-		=>
-		Inner_ParseStringExplicit_NullInput_Fails<Unit>();
+    [Fact]
+    public static void ParseStringExplicit_NullInput_Fails()
+        =>
+        Inner_ParseStringExplicit_NullInput_Fails<Unit>();
 
-	[Fact]
-	public static void TryParseStringExplicit_NullInput_Fails()
-		=>
-		Inner_TryParseStringExplicit_NullInput_Fails<Unit>();
+    [Fact]
+    public static void TryParseStringExplicit_NullInput_Fails()
+        =>
+        Inner_TryParseStringExplicit_NullInput_Fails<Unit>();
 
-	private static void Inner_ParseStringExplicit_Succeeds<TUnit>(string s)
-		where TUnit : struct, IParsable<TUnit>
-	{
-		var actual = TUnit.Parse(s, null);
+    private static void Inner_ParseStringExplicit_Succeeds<TUnit>(string s)
+        where TUnit : struct, IParsable<TUnit>
+    {
+        var actual = TUnit.Parse(s, null);
 
-		var expected = default(TUnit);
+        var expected = default(TUnit);
 
-		Assert.StrictEqual(expected, actual);
-	}
+        Assert.StrictEqual(expected, actual);
+    }
 
-	private static void Inner_TryParseStringExplicit_Succeeds<TUnit>(string s)
-		where TUnit : struct, IParsable<TUnit>
-	{
-		var actual = TUnit.TryParse(s, null, out var result);
+    private static void Inner_TryParseStringExplicit_Succeeds<TUnit>(string s)
+        where TUnit : struct, IParsable<TUnit>
+    {
+        var actual = TUnit.TryParse(s, null, out var result);
 
-		var expected = default(TUnit);
+        var expected = default(TUnit);
 
-		Assert.True(actual);
-		Assert.StrictEqual(expected, result);
-	}
+        Assert.True(actual);
+        Assert.StrictEqual(expected, result);
+    }
 
-	private static void Inner_ParseStringExplicit_NullInput_Fails<TUnit>()
-		where TUnit : struct, IParsable<TUnit>
-	{
-		_ = Assert.Throws<ArgumentNullException>("s", () => _ = TUnit.Parse(null!, null));
-	}
+    private static void Inner_ParseStringExplicit_NullInput_Fails<TUnit>()
+        where TUnit : struct, IParsable<TUnit>
+    {
+        _ = Assert.Throws<ArgumentNullException>("s", () => _ = TUnit.Parse(null!, null));
+    }
 
-	private static void Inner_TryParseStringExplicit_NullInput_Fails<TUnit>()
-		where TUnit : struct, IParsable<TUnit>
-	{
-		var actual = TUnit.TryParse(null, null, out var result);
+    private static void Inner_TryParseStringExplicit_NullInput_Fails<TUnit>()
+        where TUnit : struct, IParsable<TUnit>
+    {
+        var actual = TUnit.TryParse(null, null, out var result);
 
-		var expected = default(TUnit);
+        var expected = default(TUnit);
 
-		Assert.False(actual);
-		Assert.StrictEqual(expected, result);
-	}
+        Assert.False(actual);
+        Assert.StrictEqual(expected, result);
+    }
 }
