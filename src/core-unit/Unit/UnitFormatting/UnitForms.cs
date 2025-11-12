bbf87dbd65086b54ac760358c5041fcd71ec52bb
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-12 21:52:05 +0400

    Fix formatting (restore spaces instead of tabs)

--- src/core-unit/Unit/UnitFormatting/UnitForms.cs
+++ src/core-unit/Unit/UnitFormatting/UnitForms.cs
@@ -2,16 +2,16 @@
 
 internal static class UnitForms
 {
-	// General (default) form
-	internal const string General = CanonicalExtended;
+    // General (default) form
+    internal const string General = CanonicalExtended;
 
-	// Basic forms
-	internal const string Canonical = "()";
-	internal const string JsonObj = "{}";
-	internal const string Empty = "";
+    // Basic forms
+    internal const string Canonical = "()";
+    internal const string JsonObj = "{}";
+    internal const string Empty = "";
 
-	// Extended forms
-	internal const string CanonicalExtended = "Unit:()";
-	internal const string JsonObjExtended = "Unit:{}";
-	internal const string EmptyExtended = "Unit";
+    // Extended forms
+    internal const string CanonicalExtended = "Unit:()";
+    internal const string JsonObjExtended = "Unit:{}";
+    internal const string EmptyExtended = "Unit";
 }
