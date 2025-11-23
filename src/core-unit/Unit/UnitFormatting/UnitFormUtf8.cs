1695fcd3d7be277caf607b255aebb2430853643d
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-23 15:40:38 +0400

    Minor improvement (UnitFormUtf8)

--- src/core-unit/Unit/UnitFormatting/UnitFormUtf8.cs
+++ src/core-unit/Unit/UnitFormatting/UnitFormUtf8.cs
@@ -5,30 +5,30 @@ namespace System;
 internal static class UnitFormUtf8
 {
     // General (default) form
-    private static readonly byte[] general = Utf8.GetBytes(UnitForm.General);
+    internal static ReadOnlySpan<byte> General => new(InnerBytes.General);
 
     // Basic forms
-    private static readonly byte[] canonical = Utf8.GetBytes(UnitForm.Canonical);
-    private static readonly byte[] jsonObj = Utf8.GetBytes(UnitForm.JsonObj);
-    private static readonly byte[] empty = Utf8.GetBytes(UnitForm.Empty);
+    internal static ReadOnlySpan<byte> Canonical => new(InnerBytes.Canonical);
+    internal static ReadOnlySpan<byte> JsonObj => new(InnerBytes.JsonObj);
+    internal static ReadOnlySpan<byte> Empty => new(InnerBytes.Empty);
 
     // Extended forms
-    private static readonly byte[] canonicalExtended = Utf8.GetBytes(UnitForm.CanonicalExtended);
-    private static readonly byte[] jsonObjExtended = Utf8.GetBytes(UnitForm.JsonObjExtended);
-    private static readonly byte[] emptyExtended = Utf8.GetBytes(UnitForm.EmptyExtended);
+    internal static ReadOnlySpan<byte> CanonicalExtended => new(InnerBytes.CanonicalExtended);
+    internal static ReadOnlySpan<byte> JsonObjExtended => new(InnerBytes.JsonObjExtended);
+    internal static ReadOnlySpan<byte> EmptyExtended => new(InnerBytes.EmptyExtended);
 
-    private static Encoding Utf8 => Encoding.UTF8;
+    private static class InnerBytes
+    {
+        internal static readonly byte[] General = Utf8.GetBytes(UnitForm.General);
 
-    // General (default) form
-    internal static ReadOnlySpan<byte> General => new(general);
+        internal static readonly byte[] Canonical = Utf8.GetBytes(UnitForm.Canonical);
+        internal static readonly byte[] JsonObj = Utf8.GetBytes(UnitForm.JsonObj);
+        internal static readonly byte[] Empty = Utf8.GetBytes(UnitForm.Empty);
 
-    // Basic forms
-    internal static ReadOnlySpan<byte> Canonical => new(canonical);
-    internal static ReadOnlySpan<byte> JsonObj => new(jsonObj);
-    internal static ReadOnlySpan<byte> Empty => new(empty);
+        internal static readonly byte[] CanonicalExtended = Utf8.GetBytes(UnitForm.CanonicalExtended);
+        internal static readonly byte[] JsonObjExtended = Utf8.GetBytes(UnitForm.JsonObjExtended);
+        internal static readonly byte[] EmptyExtended = Utf8.GetBytes(UnitForm.EmptyExtended);
 
-    // Extended forms
-    internal static ReadOnlySpan<byte> CanonicalExtended => new(canonicalExtended);
-    internal static ReadOnlySpan<byte> JsonObjExtended => new(jsonObjExtended);
-    internal static ReadOnlySpan<byte> EmptyExtended => new(emptyExtended);
+        private static Encoding Utf8 => Encoding.UTF8;
+    }
 }
