d495c2167a4eba16fc758d2a1136c028a022fe24
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-24 21:53:20 +0400

    Add a comment

--- src/core-unit/Unit/Unit/Unit.Factory.cs
+++ src/core-unit/Unit/Unit/Unit.Factory.cs
@@ -4,6 +4,9 @@ namespace System;
 
 partial struct Unit
 {
+    // This singleton value is for use when it's needed to emphasize that Unit is being used.
+    // In other cases, prefer "default(Unit)" or "new Unit()" for better performance.
+
     public static readonly Unit Value;
 
     // This factory is for use in method group conversion instead of lambda expressions
