2d056792483eba864fcc68e97fef6612a2dba5ed
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-24 23:11:32 +0400

    Add a comment

--- src/core-unit/Unit/UnitExtensions/ToUnit.cs
+++ src/core-unit/Unit/UnitExtensions/ToUnit.cs
@@ -5,6 +5,7 @@ namespace System;
 partial class UnitExtensions
 {
     // We maintain that Unit can be derived from any input value.
+    // Unit.From is being called instead of returning default(Unit) for the sake of clarity.
     //
     // This extension is for more convenient syntax when converting to Unit from a result value.
 
