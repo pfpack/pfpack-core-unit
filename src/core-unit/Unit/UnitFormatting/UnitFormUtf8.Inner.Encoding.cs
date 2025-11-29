12636324a8fc4bf90fe0c76a8ca32a2019e91706
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-11-29 16:05:21 +0400

    UnitFormUtf8: force inlining InnerGetBytes

--- src/core-unit/Unit/UnitFormatting/UnitFormUtf8.Inner.Encoding.cs
+++ src/core-unit/Unit/UnitFormatting/UnitFormUtf8.Inner.Encoding.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace System;
 
@@ -6,6 +7,7 @@ partial class UnitFormUtf8
 {
     // We use the most proven method to obtain UTF-8 bytes.
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static byte[] InnerGetBytes(string s)
         =>
         Encoding.UTF8.GetBytes(s);
