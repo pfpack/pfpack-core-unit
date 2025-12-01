2624984887c688bb82797d7c195b46f1ac3deb25
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-12-01 22:58:37 +0400

    Revert refactor / improve diff

--- src/core-unit/Unit/UnitFormatting/UnitFormUtf8.cs
+++ src/core-unit/Unit/UnitFormatting/UnitFormUtf8.cs
@@ -2,16 +2,4 @@
 
 internal static partial class UnitFormUtf8
 {
-    // General (default) form
-    internal static ReadOnlySpan<byte> General => InnerAsSpan(InnerGeneral.Value);
-
-    // Basic forms
-    internal static ReadOnlySpan<byte> Canonical => InnerAsSpan(InnerCanonical.Value);
-    internal static ReadOnlySpan<byte> JsonObj => InnerAsSpan(InnerJsonObj.Value);
-    internal static ReadOnlySpan<byte> Empty => InnerAsSpan(InnerEmpty.Value);
-
-    // Extended forms
-    internal static ReadOnlySpan<byte> CanonicalExtended => InnerAsSpan(InnerCanonicalExtended.Value);
-    internal static ReadOnlySpan<byte> JsonObjExtended => InnerAsSpan(InnerJsonObjExtended.Value);
-    internal static ReadOnlySpan<byte> EmptyExtended => InnerAsSpan(InnerEmptyExtended.Value);
 }
